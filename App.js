e861100af160eaecf3175acd7b7a9a8592d7e112
Aimho <jjhwhwnsgh@gmail.com>
2021-02-02 11:03:09 +0900

    add webview code / add readme

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  // open webview url
+  const url = `https://www.google.com/`;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "#000",
+      }}
+    >
+      <WebView source={{ url }} />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,4 @@
+### toy-webview 개요
+
+- android, ios 기기에서 webview test하기 위한 용도로 만듦
+- `App.js`에서 url 변경 후 실행하면 됨
